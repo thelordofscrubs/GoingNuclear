6192a110bdeda9c20243c1f37daa9cd0ccdf57c7
thelordofscrubs <ekaituri@gmail.com>
2025-01-29 17:16:34 -0600

    started pre day shop

---
+++ Game/DayStart/DayStart.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+public partial class DayStart : Control
+{
+	public Dictionary<string, Label> ShopLabels = new Dictionary<string, Label>();
+
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public void FindLabels() {
+		
+	}
+
+}
